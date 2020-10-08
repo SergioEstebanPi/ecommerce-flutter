236c392335120d57d36e857f3e7283fa6c77587d
Sergio <sepi_147@hotmail.com>
2020-10-07 21:55:44 -0500

    Ajusto el uso de Firebase y agrego estilos a la pantalla de login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'home.dart';
@@ -13,7 +13,7 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   final GoogleSignIn googleSignIn = new GoogleSignIn();
-  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  final auth.FirebaseAuth firebaseAuth = auth.FirebaseAuth.instance;
   SharedPreferences preferences;
   bool loading = false;
   bool isLogedin = false;
@@ -22,24 +22,6 @@ class _LoginState extends State<Login> {
     super.initState();
     isSignedIn();
   }
-  void isSignedIn() async {
-    setState((){
-      loading = true;
-    });
-    preferences = await SharedPreferences.getInstance();
-    isLogedin = await googleSignIn.isSignedIn();
-    if(isLogedin){
-      Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-              builder: (context) => HomePage(),
-          )
-      );
-    }
-    setState((){
-      loading: false;
-    });
-  }
   Future handleSignIn() async{
     preferences = await SharedPreferences.getInstance();
     setState(() {
@@ -47,31 +29,35 @@ class _LoginState extends State<Login> {
     });
     GoogleSignInAccount googleUser = await googleSignIn.signIn();
     GoogleSignInAuthentication googleSignInAuthentication = await googleUser.authentication;
-    final AuthCredential credential = GoogleAuthProvider.getCredential(
+    final auth.AuthCredential credential = auth.GoogleAuthProvider.credential(
       accessToken: googleSignInAuthentication .accessToken,
       idToken: googleSignInAuthentication.idToken,
     );
-    FirebaseUser firebaseUser = (await firebaseAuth.signInWithCredential(credential)) as FirebaseUser;
+    auth.UserCredential userCredential = await firebaseAuth.signInWithCredential(credential);
+    auth.User firebaseUser = userCredential.user;
+    Fluttertoast.showToast(msg: "" + userCredential.toString());
+    Fluttertoast.showToast(msg: "" + firebaseUser.displayName);
+    //User firebaseUser = () as User;
     if(firebaseUser != null){
-      final QuerySnapshot result = await Firestore.instance
+      final QuerySnapshot result = await FirebaseFirestore.instance
           .collection("users")
           .where(
             "id",
             isEqualTo: firebaseUser.uid
-        ).getDocuments();
-      final List<DocumentSnapshot> documents = result.documents;
+        ).get();
+      final List<DocumentSnapshot> documents = result.docs;
       if(documents.length == 0) {
         // insert the user to our collection
-        Firestore.instance.collection("users")
-            .document(firebaseUser.uid)
-            .setData({
+        FirebaseFirestore.instance.collection("users")
+            .doc(firebaseUser.uid)
+            .set({
               "id": firebaseUser.uid,
               "username": firebaseUser.displayName,
-              "profilePicture": firebaseUser.photoUrl
+              "profilePicture": firebaseUser.photoURL
             });
         await preferences.setString("id", firebaseUser.uid);
         await preferences.setString("username", firebaseUser.displayName);
-        await preferences.setString("profilePicture", firebaseUser.photoUrl);
+        await preferences.setString("profilePicture", firebaseUser.photoURL);
       } else {
         await preferences.setString("id", documents[0]['id']);
         await preferences.setString("username", documents[0]['username']);
@@ -83,25 +69,70 @@ class _LoginState extends State<Login> {
       });
 
     } else {
-
+      Fluttertoast.showToast(msg: "You are not logged");
     }
+  }
+  void isSignedIn() async {
+    Fluttertoast.showToast(msg: "Firebase login");
+    preferences = await SharedPreferences.getInstance();
+    isLogedin = await googleSignIn.isSignedIn();
+    if(isLogedin){
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+              builder: (context) => HomePage(),
+          )
+      );
+    } else {
+      Fluttertoast.showToast(msg: "You are not lg");
+    }
+    setState(() {
+      loading: false;
+    });
   }
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Container(
-          child: Text('Registration'),
-          padding: const EdgeInsets.all(16),
-          alignment: Alignment.center,
+    return Scaffold(
+      appBar:  AppBar(
+      elevation: 0.1,
+      backgroundColor: Colors.white,
+      centerTitle: true,
+      title: Text(
+        'Login',
+        style: TextStyle(
+          color: Colors.red.shade900
         ),
-        Container(
-          child: Text('Sign In'),
-          padding: const EdgeInsets.all(16),
-          alignment: Alignment.center,
-        ),
-      ],
+      ),
+      ),
+      body: Stack(
+        children: [
+          Center(
+            child: FlatButton(
+              color: Colors.red.shade900,
+              onPressed: () {
+                handleSignIn();
+                Fluttertoast.showToast(msg: "click");
+              },
+              child: Text(
+                'Sign In/ Sign Up with Google',
+                style: TextStyle(
+                  color: Colors.white
+                ),
+              ),
+
+            ),
+          ),
+          Visibility(
+            visible: loading ?? true,
+            child: Container(
+              color: Colors.white.withOpacity(0.7),
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation(Colors.red),
+              ),
+            )
+          )
+        ],
+      ),
     );
   }
 }
